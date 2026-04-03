8b109d2e1eb940ac761b961d6bea5b62c416c717
Diego Osuna <7rgsx7d6jz@privaterelay.appleid.com>
2026-04-03 16:09:20 +0000

    initial commit for models

--- src/api/models.py
+++ src/api/models.py
@@ -1,6 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import String, Boolean
 from sqlalchemy.orm import Mapped, mapped_column
+import { useDiscountCalculator } from "../hooks/useDiscountCalculator";
+import DiscountPage from "../components/DiscountPage";
 
 db = SQLAlchemy()
 
@@ -17,3 +19,10 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+    
+class Business(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    business_name: Mapped[str] = mapped_column(String(120))
+    business_phone_number: Mapped[str] = mapped_column(String(120))
+    business_address: Mapped[str] = mapped_column(String(120))
+    business_discounts: Mapped["discounts"] = relationship(back_populates="discounts")

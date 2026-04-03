10c96e99ad29ccec4b21877b6c0c877de8cafd74
Erick de los Reyes <delosreyeserick90@gmail.com>
2026-04-03 20:46:39 +0000

    First version of navbar

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -1,19 +1,45 @@
 import { Link } from "react-router-dom";
+import logoImageUrl from "../assets/img/logo.png";
 
 export const Navbar = () => {
 
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary border border-2 border-dark border-buttom shadow">
+      <div className="container-fluid fs-5 fw-medium">
+        <img src={logoImageUrl} className="navbar-brand" href="#"></img>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav  ms-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Services
+              </a>
+              <ul className="dropdown-menu">
+                <li><a className="dropdown-item" href="#">Restaurants</a></li>
+                <li><a className="dropdown-item" href="#">Home Improve</a></li>
+                <li><hr className="dropdown-divider"></hr></li>
+                <li><a className="dropdown-item" href="#">Lawyers</a></li>
+              </ul>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">About Us</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Contact Us</a>
+            </li>g
+          </ul>
+          <form className="d-flex" role="search">
+            <input className="form-control me-2" type="search" placeholder="Search Services" aria-label="Search" />
+            <button className="btn btn-outline-warning" type="submit">Search</button>
+          </form>
+        </div>
+      </div>
+    </nav>
+  );
 };

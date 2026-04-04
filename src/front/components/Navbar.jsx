36abf5e0467ced94a38729fe2a2b34af4b85b0fe
Erick de los Reyes <delosreyeserick90@gmail.com>
2026-04-04 02:51:12 +0000

    Hero section added

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -7,13 +7,13 @@ export const Navbar = () => {
     <nav className="navbar navbar-expand-lg bg-body-tertiary border border-2 border-dark border-buttom shadow">
       <div className="container-fluid fs-5 fw-medium">
         <a className="navbar-brand" href="#">
-          <img src={logoImageUrl} style={{ width: "35gi0px"}} />
+          <img src={logoImageUrl} style={{ width: "350px"}} />
           </a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav  ms-auto mb-2 mb-lg-0">
+          <ul className="navbar-nav  ms-auto">
             <li className="nav-item">
               <a className="nav-link active" aria-current="page" href="#">Home</a>
             </li>
--- src/front/pages/Home.jsx
+++ src/front/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react"
 import rigoImageUrl from "../assets/img/rigo-baby.jpg";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
+import { Hero } from "../components/Hero.jsx";
+
 
 export const Home = () => {
 
@@ -33,8 +35,9 @@ export const Home = () => {
 	}, [])
 
 	return (
-		<div className="text-center mt-5">
-			<h1 className="display-4">Hello Rigo!!</h1>
+		
+		<div className="text-center">
+			<Hero />
 			<p className="lead">
 				<img src={rigoImageUrl} className="img-fluid rounded-circle mb-3" alt="Rigo Baby" />
 			</p>

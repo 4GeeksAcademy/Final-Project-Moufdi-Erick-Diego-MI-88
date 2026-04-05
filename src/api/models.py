e9330714a4e7e0afaa21e72698ee80199dbc657a
Diego Osuna <7rgsx7d6jz@privaterelay.appleid.com>
2026-04-05 14:38:22 +0000

    worked on models and types of business

--- src/api/models.py
+++ src/api/models.py
@@ -1,16 +1,17 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
+from sqlalchemy import String, Boolean, Enum
 from sqlalchemy.orm import Mapped, mapped_column
-import { useDiscountCalculator } from "../hooks/useDiscountCalculator";
-import DiscountPage from "../components/DiscountPage";
+
 
 db = SQLAlchemy()
+
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    
 
 
     def serialize(self):
@@ -20,9 +21,18 @@ class User(db.Model):
             # do not serialize the password, its a security breach
         }
     
+class BusinessType(enum.Enum):
+    Agriculture, Forestry, Fishing, Hunting = "agriculture, forestry, fishing & hunting"
+    Construction, Skilled_Trades = "Construction, Skilled_trades"
+    Manufacturing = "manufacturing"
+    Wholesale, Retail_Trade = "wholesale & retail trade"
+    Transportation, Warehousing = "transportation, warehousing"
+
+
 class Business(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
+    type_of_business: Mapped[BusinessType] = mapped_column(Enum(BusinessType), nullable=False) 
     business_name: Mapped[str] = mapped_column(String(120))
     business_phone_number: Mapped[str] = mapped_column(String(120))
     business_address: Mapped[str] = mapped_column(String(120))
-    business_discounts: Mapped["discounts"] = relationship(back_populates="discounts")
+    # business_discounts: Mapped["discounts"] = relationship(back_populates="discounts")

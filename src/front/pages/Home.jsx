c67d4ca1db64f81de1415888c8ac5158c6beec87
Diego Osuna <7rgsx7d6jz@privaterelay.appleid.com>
2026-04-08 22:14:37 +0000

    temp commit

--- src/front/pages/Home.jsx
+++ src/front/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react"
-import rigoImageUrl from "../assets/img/rigo-baby.jpg";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
+import { Hero } from "../components/Hero.jsx";
+import { BusinessCard } from "../components/BusinessCard.jsx";
+
 
 export const Home = () => {
 
@@ -33,20 +35,18 @@ export const Home = () => {
 	}, [])
 
 	return (
-		<div className="text-center mt-5">
-			<h1 className="display-4">Hello Rigo!!</h1>
-			<p className="lead">
-				<img src={rigoImageUrl} className="img-fluid rounded-circle mb-3" alt="Rigo Baby" />
-			</p>
-			<div className="alert alert-info">
-				{store.message ? (
-					<span>{store.message}</span>
-				) : (
-					<span className="text-danger">
-						Loading message from the backend (make sure your python 🐍 backend is running)...
-					</span>
-				)}
+		
+		<div className="text-center">
+			<Hero />
+			<div className="container">
+			<div className="d-flex justify-content-around py-5">
+			<BusinessCard className="col-3" />
+			<BusinessCard className="col-3" />
+			<BusinessCard className="col-3" />
+			<BusinessCard className="col-3" />
 			</div>
+			</div>
+			
 		</div>
 	);
 }; 

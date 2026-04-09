ae2e98cc6ccab20f216cfe2df01185afafbe2570
Diego Osuna <7rgsx7d6jz@privaterelay.appleid.com>
2026-04-09 23:50:12 +0000

    about us page created

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -14,6 +14,7 @@ import { LogIn } from "./pages/LogInPage";
 import { BusinessSignUp } from "./pages/BusinessSignUpPage";
 import { BusinessPageProfile } from "./pages/BusinessPageProfile";
 import { DiscountPage } from "./components/DiscountPage";
+import  AboutUs from "./pages/AboutUsPage";
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
@@ -35,6 +36,7 @@ export const router = createBrowserRouter(
         <Route path="/bussiness-signup" element={<BusinessSignUp/>} />
         <Route path="/business/:id" element={<BusinessPageProfile />} />
         <Route path="/business/:id/discounts" element={<DiscountPage />} />
+        <Route path="/about-us" element={<AboutUs />} />
       </Route>
     )
 );
